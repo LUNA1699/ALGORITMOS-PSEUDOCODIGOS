e63b862fe4d68ac2efbb7b7652d906ab4545b4f9
LUNA1699 <miguel_luna2716@outlook.com>
2025-03-03 10:43:08 -0600

    RETO MATEMATICO

---
+++ main.py
@@ -0,0 +1,50 @@
+print("Hello CodeSandbox!")
+print("Hola valeria")
+print("Hola Naomi")
+print("Hola Sebastian")
+print("Hola Cassandra")
+print("Hola Karol")
+print("Hola Messi")
+print("Hola Cristiano")
+print("Hola Levi")
+print("Hola Shakira")
+""" Vamos a concatenar """
+nombre = "Miguel"
+apellido = "Luna"
+print ("Hola mi nombre es:",nombre)
+print ("Hola mi nombre es:",nombre,apellido)
+print ("Hola mi nombre es:",nombre, "y mi apellido es:",apellido)#Hola mi nombre es:Kevin y mi apellido es:Luna
+
+""" Concatenacion con diferente tipos de datos """
+mi_edad=25
+mi_nombre="Miguel"
+mi_apellido_paterno= "Luna"
+mi_apellido_materno= "Hernandez"
+soy_estudiante= True
+soy_politico= False
+
+print("Hola mi edad es:",mi_edad, "mi nombre es:",mi_nombre, "mi apellido paterno es:",mi_apellido_paterno, "mi apellido materno es:",mi_apellido_materno,)
+
+""" Uso de type-Averigua que tipo de dato recibes """
+print(type(mi_edad))#<class 'int'>
+print(type(mi_nombre))#<class 'str'>
+print(type(soy_estudiante))#<class 'bool'>
+
+""" Operaciones matematicas """
+numero_uno=10
+numero_dos=5
+print(numero_uno+numero_dos)
+print(numero_uno-numero_dos)
+print(numero_uno*numero_dos)
+print(numero_uno/numero_dos)
+print(numero_uno%numero_dos)
+
+""" Mathematical operations """
+#Ask the user to enter the numbers
+number_one = int(input("Enter the first number: "))
+number_two = int(input("Enter the second number: "))
+#Perform the operation and show the result
+print(number_one + number_two) # Print the result of adding the two numbers
+print(number_one - number_two) # Print the result of subtracting the second number from the first
+print(number_one * number_two) # Print the result of multiplying the two numbers
+print(number_one / number_two) # Print the result of dividing the first number by the second
