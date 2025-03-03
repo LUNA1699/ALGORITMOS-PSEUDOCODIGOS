8dbacef19a6237b5680a3934b4cf46e988ac0c05
LUNA1699 <miguel_luna2716@outlook.com>
2025-03-03 11:11:54 -0600

    ASIGNACION

---
+++ Ejercicio1.py
@@ -0,0 +1,28 @@
+#Numero se asigna un valor a una variable
+numero=10
+
+#Se imprime el valor de la variable numero, ej. 10
+print("El numero asignado es:", numero)
+
+"""
+ Actividad de clase: definir valores de variables y las van a imprimir; sobre algun hobby usarás:
+-enteros
+-booleanos
+-caracteres
+Tiempo: 8min
+"""
+
+# Definición de variables
+# Entero: Número de meses jugando fútbol
+meses_jugando = 15  
+
+# Booleano: Indica si el fútbol es su hobby favorito
+es_hobby_favorito = True  
+
+# Caracter: Nivel de dificultad que percibe (A: Alta, M: Media, B: Baja)
+nivel_dificultad = 'B'  
+
+# Impresión de valores
+print("Tiempo jugando fútbol:", meses_jugando, "meses")
+print("¿Es el fútbol mi hobby favorito?", es_hobby_favorito)
+print("Nivel de dificultad percibido:", nivel_dificultad)
